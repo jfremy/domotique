e7cde46133ab216e8c22a0a68617b75f825a069f
Jean-Francois Remy <jeff@melix.org>
2012-07-03 00:20:51 +0200

    initial commit

---
+++ src/collector.py
@@ -0,0 +1,127 @@
+__author__ = 'Jeff'
+
+import serial
+import time
+import signal
+import sys
+
+# Close on ctrl+C
+def signal_handler(signal, frame):
+    print 'Exiting on Ctrl+C'
+    ser.close()
+    sys.exit(0)
+
+def accumulate(buffer, offset, nbr):
+    accu = 0
+    for i in range(0,nbr):
+        accu = accu*256 + buffer[offset + i];
+    return accu
+
+
+def processTempHumSensor(msg):
+    packetLength = msg[0]
+    if packetLength != 10:
+        print "Message with invalid length, got " + str(packetLength) + " expected 10"
+    subtype = msg[2]
+    seqNbr = msg[3]
+    id1 = msg[4]
+    id2 = msg[5]
+    temperature = accumulate(msg, 6, 2)
+    if temperature >= 32768:
+        temperature = -(temperature - 32768)
+    temperature /= float(10)
+
+    humidity = msg[8]
+    humidityStatus = msg[9]
+
+    print "SubType " + str(subtype)
+    print "Sequence Number " + str(seqNbr)
+    print "id1 " + str(id1)
+    print "id2 " + str(id2)
+    print "Temperature " + str(temperature) +"C"
+    print "Humidity " + str(humidity) +"%"
+    print "Humidity status " + str(humidityStatus)
+
+def processEnergyUsageSensor(msg):
+    packetLength = msg[0]
+    if packetLength != 17:
+        print "Message with invalid length, got " + str(packetLength) + " expected 17"
+    subtype = msg[2]
+    seqNbr = msg[3]
+    id1 = msg[4]
+    id2 = msg[5]
+    count = msg[6]
+    instant = accumulate(msg, 7,4)
+    total = accumulate(msg, 11,6) / float(223666) # To get kWh cf doc from rfxcom
+    print "SubType " + str(subtype)
+    print "Sequence Number " + str(seqNbr)
+    print "id1 " + str(id1)
+    print "id2 " + str(id2)
+    print "count " + str(count)
+    print "Instant power " + str(instant)
+    print "Total power " + str(total)
+
+def parseMessage(msg):
+    print 'Packet length ' + str(msg[0])
+    packetType = msg[1]
+
+    if packetType == 1:
+        print "Received interface control (0x01) message"
+    elif packetType == 2:
+        print "Received Receiver/Transmitter (0x02) message"
+    elif packetType == 3:
+        print "Received undecoded RF (0x03) message"
+    elif packetType == 80:
+        print "Received Temperature sensor (0x50) message"
+    elif packetType == 81:
+        print "Received Humidy sensor (0x51) message"
+    elif packetType == 82:
+        print "Received Temp & Humidity sensor (0x52) message"
+        processTempHumSensor(msg)
+    elif packetType == 83:
+        print "Received Barometric sensor (0x53) message"
+    elif packetType == 84:
+        print "Received Temp, Hum, Baro sensor (0x54) message"
+    elif packetType == 85:
+        print "Received Rain sensor (0x55) message"
+    elif packetType == 86:
+        print "Received Wind sensor (0x56) message"
+    elif packetType == 87:
+        print "Received UV sensor (0x57) message"
+    elif packetType == 90:
+        print "Received Energy usage sensor (0x5A) message"
+        processEnergyUsageSensor(msg)
+    else:
+        print "Unsupported packet type " + str(packetType)
+
+
+
+signal.signal(signal.SIGINT, signal_handler)
+
+ser = serial.Serial('\\.\COM3', 38400, timeout = 0.2)
+print ser
+
+# Reset command
+reset = bytearray.fromhex("0D00000000000000000000000000")
+# GetStatus command
+status = bytearray.fromhex("0D00000102000000000000000000")
+
+ser.write(reset)
+time.sleep(0.05)
+ser.flush()
+ser.write(status)
+
+buffer = bytearray(b'')
+
+while True:
+    data = bytearray(ser.read(32))
+    buffer += data
+    if len(buffer) > 0:
+        packetLength = buffer[0]
+        if len(buffer) > packetLength:
+            msg = buffer[0:packetLength+1] #size does not include size byte (so actual size is +1)
+            del buffer[0:packetLength+1] #remove message
+            parseMessage(msg)
+
+
+
