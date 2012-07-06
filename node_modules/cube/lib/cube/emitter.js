4fbbaa0734667cd9745a12b2ab26aad1ea9a4775
Jean-Francois Remy <jeff@melix.org>
2012-07-06 19:45:07 +0200

    clean-up modules, just need cube for now

---
+++ node_modules/cube/lib/cube/emitter.js
@@ -0,0 +1,16 @@
+var util = require("util"),
+    url = require("url"),
+    http = require("./emitter-http"),
+    udp = require("./emitter-udp"),
+    ws = require("./emitter-ws");
+
+module.exports = function(u) {
+  var emitter;
+  u = url.parse(u);
+  switch (u.protocol) {
+    case "udp:": emitter = udp; break;
+    case "ws:": case "wss:": emitter = ws; break;
+    case "http:": emitter = http; break;
+  }
+  return emitter(u.protocol, u.hostname, u.port);
+};
---
+++ node_modules/cube/test/test.js
@@ -0,0 +1,57 @@
+var mongodb = require("mongodb"),
+    assert = require("assert"),
+    util = require("util"),
+    http = require("http");
+
+exports.port = 1083;
+
+exports.batch = function(batch) {
+  return {
+    "": {
+      topic: function() {
+        var client = new mongodb.Server("localhost", 27017);
+            db = new mongodb.Db("cube_test", client),
+            cb = this.callback;
+        db.open(function(error) {
+          var collectionsRemaining = 2;
+          db.dropCollection("test_events", collectionReady);
+          db.dropCollection("test_metrics", collectionReady);
+          function collectionReady() {
+            if (!--collectionsRemaining) {
+              cb(null, {client: client, db: db});
+            }
+          }
+        });
+      },
+      "": batch,
+      teardown: function(test) {
+        if (test.client.isConnected()) {
+          test.client.close();
+        }
+      }
+    }
+  };
+};
+
+exports.request = function(options, data) {
+  return function() {
+    var cb = this.callback;
+
+    options.host = "localhost";
+
+    var request = http.request(options, function(response) {
+      response.body = "";
+      response.setEncoding("utf8");
+      response.on("data", function(chunk) { response.body += chunk; });
+      response.on("end", function() { cb(null, response); });
+    });
+
+    request.on("error", function(e) { cb(e, null); });
+
+    if (data && data.length > 0) request.write(data);
+    request.end();
+  };
+};
+
+// Disable logging for tests.
+util.log = function() {};
